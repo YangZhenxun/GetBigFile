3c66758cf516cb32ca8c43184d53fca5a2691e17
YangZhenxun <2210067895@qq.com>
2024-07-31 17:44:22 +0800

    Update lib.rs

--- new proj/plugins/Core/cxx_usn_journal/src/lib.rs
+++ new proj/plugins/Core/cxx_usn_journal/src/lib.rs
@@ -1,14 +1,55 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+
+extern crate libc;
+use std::os::raw::c_void;
+
+use pyo3::prelude::*;
+
+use libc::{c_char, c_ulong};
+
+pub type HANDLE = *mut c_void;
+pub type LPCSTR = *const c_char;
+pub type PCSTR = *const c_char;
+pub type DWORD = *const c_ulong;
+pub type LPVOID = *mut c_void;
+
+#[repr(C)]
+pub struct _SECURITY_ATTRIBUTES {
+    pub nLength: DWORD,
+    pub lpSecurityDescriptor: LPVOID,
+    pub bInheritHandle: bool,
+} 
+pub type SECURITY_ATTRIBUTES = _SECURITY_ATTRIBUTES;
+pub type PSECURITY_ATTRIBUTES = *mut _SECURITY_ATTRIBUTES;
+pub type LPSECURITY_ATTRIBUTES = *mut _SECURITY_ATTRIBUTES;
+
+
+extern {
+    pub fn _CreateFileA(
+        lpFileName: LPCSTR,
+        dwDesiredAccess: DWORD,
+        dwShareMode: DWORD,
+        lpSecurityAttributes: LPSECURITY_ATTRIBUTES,
+        dwCreationDisposition: DWORD,
+        dwFlagsAndAttributes: DWORD,
+        hTemplateFile: LPCSTR,
+    ) -> HANDLE;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[pyfunction]
+pub fn CreateFileA(
+    lpFileName: LPCSTR,
+    dwDesiredAccess: DWORD,
+    dwShareMode: DWORD,
+    lpSecurityAttributes: LPSECURITY_ATTRIBUTES,
+    dwCreationDisposition: DWORD,
+    dwFlagsAndAttributes: DWORD,
+    hTemplateFile: LPCSTR,
+) -> PyResult<HANDLE>
+{
+    unsafe{
+        Ok(_CreateFileA(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile))    
     }
 }

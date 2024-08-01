e45d0f67133418e186ef27b063601bcce9bbfccd
YangZhenxun <2210067895@qq.com>
2024-08-01 10:48:07 +0800

    Update win32.h

--- new proj/plugins/Core/c_usn_journal/usn/win32.h
+++ new proj/plugins/Core/c_usn_journal/usn/win32.h
@@ -3,5 +3,19 @@
 #include <fileapi.h>
 
 PyObject *wrapCreateFileA(PyObject *self, PyObject *args){
-    
+    _In_ LPCSTR lpFileName;
+    _In_ DWORD dwDesiredAccess;
+    _In_ DWORD dwShareMode;
+    _In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes;
+    _In_ DWORD dwCreationDisposition;
+    _In_ DWORD dwFlagsAndAttributes;
+    _In_opt_ HANDLE hTemplateFile;
+    if (!PyArg_VaParse(args, "s", lpFileName,
+        dwDesiredAccess, 
+        dwShareMode, 
+        lpSecurityAttributes, 
+        dwCreationDisposition, 
+        dwFlagsAndAttributes,
+        hTemplateFile))
+        return NULL;
 }

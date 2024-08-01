7c1f82095ad8be444b4433cfb3dd50656e112fb0
YangZhenxun <2210067895@qq.com>
2024-08-01 09:33:00 +0800

    Update lib.rs

--- new proj/plugins/Core/cxx_usn_journal/src/lib.rs
+++ new proj/plugins/Core/cxx_usn_journal/src/lib.rs
@@ -3,11 +3,10 @@
 #![allow(non_camel_case_types)]
 
 extern crate libc;
-use std::os::raw::c_void;
 
 use pyo3::prelude::*;
 
-use libc::{c_char, c_ulong};
+use libc::{c_char, c_ulong, c_void};
 
 pub type HANDLE = *mut c_void;
 pub type LPCSTR = *const c_char;
@@ -15,16 +14,32 @@ pub type PCSTR = *const c_char;
 pub type DWORD = *const c_ulong;
 pub type LPVOID = *mut c_void;
 
+struct wrapperHandle(HANDLE, PyObject);
+
+impl IntoPy<PyObject> for wrapperHandle {
+    fn into_py(self, py: Python<'_>) -> PyObject {
+        self.1
+    }
+}
+#[pyclass]
 #[repr(C)]
 pub struct _SECURITY_ATTRIBUTES {
     pub nLength: DWORD,
     pub lpSecurityDescriptor: LPVOID,
     pub bInheritHandle: bool,
 } 
+/*
+struct wrapperSA(_SECURITY_ATTRIBUTES, PyObject);
+
+impl IntoPy<PyObject> for wrapperSA {
+    fn into_py(self, py: Python<'_>) -> PyObject {
+        self.1
+    }
+}*/
+
 pub type SECURITY_ATTRIBUTES = _SECURITY_ATTRIBUTES;
 pub type PSECURITY_ATTRIBUTES = *mut _SECURITY_ATTRIBUTES;
 pub type LPSECURITY_ATTRIBUTES = *mut _SECURITY_ATTRIBUTES;
-
 
 extern {
     pub fn _CreateFileA(

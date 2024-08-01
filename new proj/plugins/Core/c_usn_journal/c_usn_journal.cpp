bd1347c566d44e0c13fc7c5c0d85efd65675bcf6
YangZhenxun <2210067895@qq.com>
2024-08-01 10:18:13 +0800

    cxxs

--- new proj/plugins/Core/c_usn_journal/c_usn_journal.cpp
+++ new proj/plugins/Core/c_usn_journal/c_usn_journal.cpp
@@ -1,1 +1,5 @@
-#include <Python.h>
+#ifdef _WIN32
+    #include <usn/win32.h>
+#elif __APPLE__
+    #include <usn/darwin.h>
+#endif
---
+++ new proj/plugins/Core/c_usn_journal/usn/win32.h
@@ -0,0 +1,7 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <fileapi.h>
+
+PyObject *wrapCreateFileA(PyObject *self, PyObject *args){
+    
+}
